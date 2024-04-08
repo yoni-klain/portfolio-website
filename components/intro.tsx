7510cf341daf44032c9a53f7cacf32a6a7f82c2f
Yoni Klain <yoni.klain@gmail.com>
2024-04-08 16:16:08 +0300

    changed intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Yoni Klain.</span> I'm a{" "}
+        <span className="font-bold">technial group leader</span> with{" "}
+        <span className="font-bold">more than 20 years</span> of experience. I enjoy
+        building <span className="italic">strong technical teams & lead complicated projects</span>. My focus is{" "}
+        <span className="underline">to be a leader, not just a manager </span>.
       </motion.h1>
 
       <motion.div
