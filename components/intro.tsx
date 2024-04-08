e7d8fe1b2c276b5edd0a6f6d2222e1ecf0cf57f1
yoni-klain <162155284+yoni-klain@users.noreply.github.com>
2024-04-08 16:19:37 +0300

    Make portrait image bigger in intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -32,9 +32,10 @@ export default function Intro() {
           >
             <Image
               src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
-              width="192"
-              height="192"
+              alt="Yoni Klain portrait"
+              width="384" // Made the image twice as big
+              height="384" // Made the image twice as big
+              quality="95"
               quality="95"
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
